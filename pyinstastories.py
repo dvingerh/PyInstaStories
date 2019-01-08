4b5f34e06718d4ccb934c41715a02d269a7f4a7a
Cammy <neus2benen@gmail.com>
2019-01-08 20:50:29 +0100

    Bump version

--- pyinstastories.py
+++ pyinstastories.py
@@ -30,7 +30,7 @@ from instagram_private_api import ClientError
 from instagram_private_api import ClientThrottledError
 from instagram_private_api import Client, ClientCompatPatch
 
-script_version = "1.7"
+script_version = "1.8"
 python_version = sys.version.split(' ')[0]
 
 ##Login
