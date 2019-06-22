69fbb1ceb631ed987319d01a11b3cd136cc7b307
Cammy <neus2benen@gmail.com>
2019-06-22 21:58:08 +0200

    Bump version to 2.4

--- pyinstastories.py
+++ pyinstastories.py
@@ -31,7 +31,7 @@ except ImportError:
 from instagram_private_api import ClientError
 from instagram_private_api import Client
 
-script_version = "2.3"
+script_version = "2.4"
 python_version = sys.version.split(' ')[0]
 
 
